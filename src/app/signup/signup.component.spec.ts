12289dfed8f6a34248216e64d5bdb39584d3a86e
Mole-Miner <hugehillvv@gmail.com>
2022-03-13 20:24:37 +0200

    spec for signup successful

--- src/app/signup/signup.component.spec.ts
+++ src/app/signup/signup.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from '@core/services';
 
 import { SignupComponent } from './signup.component';
 
@@ -8,7 +11,9 @@ describe('SignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [SignupComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [AuthenticationService]
     })
     .compileComponents();
   });
