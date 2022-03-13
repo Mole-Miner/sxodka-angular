4f8ac0d04ba0d44da5c1b9ec072b538302c9aea5
Mole-Miner <hugehillvv@gmail.com>
2022-03-13 17:50:15 +0200

    spec test for authentication interceptor successful

--- src/app/core/interceptors/authentication.interceptor.spec.ts
+++ src/app/core/interceptors/authentication.interceptor.spec.ts
@@ -1,12 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { AuthenticationService } from '@core/services';
 
 import { AuthenticationInterceptor } from './authentication.interceptor';
 
 describe('AuthenticationInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthenticationInterceptor
-      ]
+    imports: [HttpClientModule],
+    providers: [AuthenticationInterceptor, AuthenticationService]
   }));
 
   it('should be created', () => {
