18bc7950f3a6a1d56b00e6d7492add0b3661b1d8
Mole-Miner <hugehillvv@gmail.com>
2022-03-13 17:11:44 +0200

    created signin form, signin request temporary disabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +14,6 @@ import { AuthenticationService } from '@core/services';
 import { AuthenticationGuard } from '@core/guards';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-
 
 @NgModule({
   declarations: [
@@ -21,7 +25,13 @@ import { SignupComponent } from './signup/signup.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   providers: [
     AuthenticationService,
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@core/services';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent implements OnInit, OnDestroy {
+  private readonly _unsubscribe: Subject<void> = new Subject<void>();
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit(): void {
+  hidePassword = true;
+
+  constructor(
+    private readonly _router: Router,
+    private readonly _authenticationService: AuthenticationService
+  ) { }
+
+  get emailFormControl(): AbstractControl {
+    return this.form.get('email')!;
   }
 
+  get passwordFormControl(): AbstractControl {
+    return this.form.get('password')!;
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(undefined, [Validators.required, Validators.email]),
+      password: new FormControl(undefined, [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  submitForm(): void {
+    if (this.form.valid) {
+      // this._authenticationService.signin(this.form.getRawValue())
+      //   .pipe(takeUntil(this._unsubscribe))
+      //   .subscribe(() => this._router.navigate(['']))
+      console.log('signin', this.form.getRawValue());
+    }
+  }
+
+  navigateToSignup(): void {
+    this._router.navigate(['signup']);
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+    this._unsubscribe.complete();
+  }
 }
